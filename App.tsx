f488267329e61ec29928774960ef90c9a170a5bf
Ruann3res <ruanpablo1218@gmail.com>
2023-06-07 13:28:58 -0300

    feat: definindo e exportando o tema da aplicaçao

--- App.tsx
+++ App.tsx
@@ -1,8 +1,12 @@
 import { Groups } from "@screens/Groups";
+import theme from "@theme/index";
+import { ThemeProvider } from "styled-components";
 
 export default function App() {
   return (
+    <ThemeProvider theme={theme}>
     <Groups/>
+    </ThemeProvider>
   );
 }
 
