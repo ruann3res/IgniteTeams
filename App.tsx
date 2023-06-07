a4700204df7f52051e0a457f4c73e64c860188d8
Ruann3res <ruanpablo1218@gmail.com>
2023-06-07 14:46:42 -0300

    feat: adiçao do expo google fonts, para utilizar a fonte roboto

--- App.tsx
+++ App.tsx
@@ -1,12 +1,18 @@
-import { Groups } from "@screens/Groups";
-import theme from "@theme/index";
-import { ThemeProvider } from "styled-components";
-
+import {
+  Roboto_400Regular,
+  Roboto_700Bold,
+  useFonts,
+} from '@expo-google-fonts/roboto'
+import { Groups } from '@screens/Groups'
+import theme from '@theme/index'
+import { ActivityIndicator } from 'react-native'
+import { ThemeProvider } from 'styled-components'
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
+
   return (
     <ThemeProvider theme={theme}>
-    <Groups/>
+      {fontsLoaded ? <Groups /> : <ActivityIndicator />}
     </ThemeProvider>
-  );
+  )
 }
-
