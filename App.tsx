843360ed805a7e004618a1f048f8b8bb67574772
Ruann3res <ruanpablo1218@gmail.com>
2023-06-07 15:01:16 -0300

    feat: adiçao do componente de load na pagina inicial

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import { Loading } from '@components/Loading'
 import {
   Roboto_400Regular,
   Roboto_700Bold,
@@ -5,14 +6,14 @@ import {
 } from '@expo-google-fonts/roboto'
 import { Groups } from '@screens/Groups'
 import theme from '@theme/index'
-import { ActivityIndicator } from 'react-native'
 import { ThemeProvider } from 'styled-components'
+
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
 
   return (
     <ThemeProvider theme={theme}>
-      {fontsLoaded ? <Groups /> : <ActivityIndicator />}
+      {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   )
 }
