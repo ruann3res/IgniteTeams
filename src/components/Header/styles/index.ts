465177f6bb3b6f9be26a4a077ef3746c6cbbb70a
Ruann3res <ruanpablo1218@gmail.com>
2023-06-10 12:20:49 -0300

    feat: adiçao da biblioteca de icone & utilizaçao dela

--- src/components/Header/styles/index.ts
+++ src/components/Header/styles/index.ts
@@ -1,5 +1,5 @@
+import { CaretLeft } from 'phosphor-react-native'
 import styled from 'styled-components/native'
-
 export const Container = styled.View`
   width: 100%;
   flex-direction: row;
@@ -11,3 +11,12 @@ export const Logo = styled.Image`
   width: 46px;
   height: 55px;
 `
+export const BackButton = styled.TouchableOpacity`
+  flex: 1;
+  background-color: ${({ theme }) => theme.COLORS.GREEN_500};
+`
+
+export const BackIcon = styled(CaretLeft).attrs(({ theme }) => ({
+  size: 36,
+  color: theme.COLORS.WHITE,
+}))``
